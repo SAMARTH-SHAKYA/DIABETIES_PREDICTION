3cdd68f7d1ac6a96c06b6b524f471cadc32adc13
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2024-11-07 18:23:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import pandas as pd
 st.title('Diabeties prediction app')
 
 st.info('Help to predict diabeties')
-data = pd.read_csv('https://github.com/SAMARTH-SHAKYA/DIABETIES_PREDICTION/blob/master/README.md')
+data = pd.read_csv('https://github.com/SAMARTH-SHAKYA/DIABETIES_PREDICTION/blob/master/diabetes.csv')
 data
